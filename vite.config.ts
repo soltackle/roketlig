bd37b4806f7c2346654767f87aa8f2bd42dcb38a
Senin Adın <senin-mail@adresin.com>
2026-06-25 00:47:26 +0300

    chore: setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/roketlig/',
   plugins: [react()],
   build: {
     target: 'esnext',
